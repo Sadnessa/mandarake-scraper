d54b685ebd9d6593a0cb3fb163c008e52a68b761
Darya Volkova <anrich@bk.ru>
2022-03-12 08:39:31 +0200

    Tested tailwind classes.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1 class="text-3xl font-bold text-green-300">
+    Hello world!
+  </h1>
 </template>
+
 
 <style>
 #app {
