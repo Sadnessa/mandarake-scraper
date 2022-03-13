e7765a0b43bd19bef7496e2169678fec7598af57
Darya Volkova <anrich@bk.ru>
2022-03-13 06:26:08 +0200

    Fixes,

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,13 @@
 
 <style lang="scss">
   @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700");
-
+  
   * {
     font-family: "Roboto", sans-serif;
+  }
+
+  body {
+    @apply bg-neutral-200;
+    @apply px-60;
   }
 </style>
