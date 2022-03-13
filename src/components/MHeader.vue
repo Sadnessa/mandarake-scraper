2e93dd87674c958a930dbb9b5ccee2119cddca99
Darya Volkova <anrich@bk.ru>
2022-03-13 06:26:36 +0200

    Added title component and structure of the site.

---
+++ src/components/MHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <h1 class="title">Welcome, user</h1>
+    <div class="btns">
+      <MButton>Log in</MButton>
+      <MButton>Log in</MButton>
+    </div>
+  </div>
+</template>
+
+<script>
+import MButton from "./base/MButton.vue";
+export default {
+  components: {
+    MButton,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  @apply bg-white;
+  @apply flex justify-between;
+  @apply py-5 px-8 rounded-b-2xl;
+
+  .title {
+    @apply flex items-center;
+    @apply font-bold text-3xl;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,21 @@
 <template>
-    <h1 class="text-3xl font-bold text-cyan-600">
-    Hello world!
-  </h1>
-  <MButton size="large" disabled>УЪУ</MButton>
-  <MButton size="small">Delete</MButton>
-  <MInput ph="Paste link here"></MInput>
+    <MHeader></MHeader>
+    <MButton size="large" disabled>УЪУ</MButton>
+    <MButton size="small">Delete</MButton>
+    <MInput placeholder="Paste link here"></MInput>
+
 </template>
 
 <script>
-import MButton from '../components/base/MButton.vue'
-import MInput from '../components/base/MInput.vue'
+import MButton from "../components/base/MButton.vue";
+import MInput from "../components/base/MInput.vue";
+import MHeader from "../components/MHeader.vue";
 
 export default {
   components: {
     MButton,
     MInput,
+    MHeader,
   },
 };
 </script>
