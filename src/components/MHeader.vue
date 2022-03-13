ef38cd681726aae668ed077d6f8dad041169b202
Darya Volkova <anrich@bk.ru>
2022-03-13 06:43:29 +0200

    Added changes to header component.

--- src/components/MHeader.vue
+++ src/components/MHeader.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="header">
     <h1 class="title">Welcome, user</h1>
-    <div class="btns">
-      <MButton>Log in</MButton>
-      <MButton>Log in</MButton>
+    <div class="btns-col">
+      <MButton>Login</MButton>
+      <MButton>Sign up</MButton>
     </div>
   </div>
 </template>
@@ -22,10 +22,17 @@ export default {
   @apply bg-white;
   @apply flex justify-between;
   @apply py-5 px-8 rounded-b-2xl;
+  @apply shadow;
 
   .title {
     @apply flex items-center;
     @apply font-bold text-3xl;
+  }
+
+  .btns-col {
+    .btn:first-child {
+      @apply mr-2;
+    }
   }
 }
 </style>
