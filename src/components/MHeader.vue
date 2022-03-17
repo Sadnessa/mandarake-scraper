1892c7fd28cc69a0c78391a19bae7241225b2122
Darya Volkova <anrich@bk.ru>
2022-03-17 04:34:39 +0200

    Added responsive design for header component.

--- src/components/MHeader.vue
+++ src/components/MHeader.vue
@@ -24,11 +24,13 @@ export default {
 .header {
   @apply bg-white;
   @apply flex justify-between items-center;
+  @apply flex-col sm:flex-row;
   @apply py-5 px-8 rounded-b-2xl;
   @apply shadow;
 
   h1 {
     @apply font-bold text-3xl;
+    @apply mb-2.5 sm:mb-0;
   }
 
   .btns-col {
