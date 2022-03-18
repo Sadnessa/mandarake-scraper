8da8d5010a2c2aafd14bf5bac6255ade8f6a1417
Darya Volkova <anrich@bk.ru>
2022-03-18 05:11:12 +0200

    Added transition for router.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
-  <router-view></router-view>
+  <router-view v-slot="{ Component }">
+    <Transition name="fade">
+      <component :is="Component" />
+    </Transition>
+  </router-view>
 </template>
 
 <style lang="scss">
-  @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700");
-  
-  * {
-    font-family: "Roboto", sans-serif;
-  }
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700");
 
-  #app {
-    @apply container 2xl:px-44 px-2;
-  }
+* {
+  font-family: "Roboto", sans-serif;
+}
 
-  body {
-    @apply bg-neutral-200;
-    
-  }
+#app {
+  @apply container 2xl:px-44 px-2;
+}
+
+body {
+  @apply bg-neutral-200;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
