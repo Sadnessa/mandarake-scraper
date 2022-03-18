24f6317182e0fa9df4b5a3c24b8609b273ea88a1
Darya Volkova <anrich@bk.ru>
2022-03-18 12:21:54 +0200

    Changed sign up.

---
+++ src/views/SignUp.vue
@@ -0,0 +1,28 @@
+<template>
+   <div class="signup">
+    <h1>Mandarake scraper</h1>
+    <MSignupCard></MSignupCard>
+  </div>
+</template>
+
+<script>
+import MSignupCard from '../components/MSignupCard.vue'
+
+export default {
+  components: {
+    MSignupCard
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.signup {
+  @apply h-screen;
+  @apply flex flex-col justify-center items-center;
+
+  h1 {
+    @apply font-bold text-4xl;
+    @apply mb-4;
+  }
+}
+</style>
