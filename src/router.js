e79e73c838c196cb044f6ee1522609a9ffd94f82
Darya Volkova <anrich@bk.ru>
2022-03-24 18:12:27 +0200

    Router changes.

--- src/router.js
+++ src/router.js
@@ -8,6 +8,12 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+    // children: [
+    //   {
+    //     path: '/list',
+    //     name: 'list',
+    //   }
+    // ]
   },
 
   {
