7b335471fd240587f90766bdff2bb77b4118e615
Darya Volkova <kequeue.8@gmail.com>
2022-04-18 07:11:50 +0300

    Added base color theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,30 @@ module.exports = {
       container: {
         center: true,
       },
+
+      textColor: {
+        skin: {
+          base: 'var(--color-text-base)',
+          secondary: 'var(--color-text-secondary)',
+          'text-button': 'var(--color-text-button)',
+          'text-button-flat': 'var(--color-text-button-flat)',
+        }
+      },
+
+      backgroundColor: {
+        skin: {
+          fill: 'var(--color-fill)',
+          'button-flat': 'var(--color-button-flat)',
+          'button-flat-hover': 'var(--color-button-flat-hover)',
+        }
+      },
+
+      borderColor: {
+        skin: {
+          'button-fill': 'var(--color-button-border)',
+          'button-flat': 'var(--color-button-flat-border)',
+        }
+      }
     },
   },
   plugins: [],
