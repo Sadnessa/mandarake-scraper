9652dfd004e469aeb72d7b5d78e37613ee630230
Darya Volkova <kequeue.8@gmail.com>
2022-04-19 16:31:17 +0300

    Added dark theme. Deleted trash code.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,21 +8,35 @@ module.exports = {
       container: {
         center: true,
       },
+      // backgroundOpacity: ['active'],
 
       textColor: {
         skin: {
+          'text-button-flat': 'var(--color-text-button-flat)',
+        },
+
+        mode: {
           base: 'var(--color-text-base)',
           secondary: 'var(--color-text-secondary)',
           'text-button': 'var(--color-text-button)',
-          'text-button-flat': 'var(--color-text-button-flat)',
         }
       },
 
       backgroundColor: {
         skin: {
-          fill: 'var(--color-fill)',
           'button-flat': 'var(--color-button-flat)',
-          'button-flat-hover': 'var(--color-button-flat-hover)',
+          fill: ({ opacityValue }) => {
+            if (opacityValue !== undefined) {
+              return `rgba(var(--color-fill), ${opacityValue})`
+            }
+            return `rgb(var(--color-fill))`
+          },
+        },
+
+        mode: {
+          card: 'var(--color-card)',
+          bg: 'var(--color-bg)',
+          input: 'var(--color-input)',
         }
       },
 
